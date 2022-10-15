3138f46d27e087b1036d46d8b33005e250904f30
gungun jain <jaingungun249@gmail.com>
2022-10-15 14:01:25 -0400

    GUI

---
+++ day20_treadmill_gui.py
@@ -0,0 +1,95 @@
+from matplotlib import image
+from numpy import imag
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tkinter as tkt
+from PIL import ImageTk,Image
+
+data = pd.read_csv('treadmil-users.csv')
+
+
+app = tkt.Tk()
+app.geometry('600x300')
+app.title('Treadmill Users Analysis')
+
+graphs = tkt.Variable(app)
+values = {
+    'Pair Plot':"sns.pairplot(data=data)",
+    'Joint Plot': "sns.jointplot(data=data, x='{col1}', y='{col2}')",
+    'Bar Plot' : "sns.barplot(data=data, x='{col1}', y='{col2}')",
+    'Box Plot':"sns.boxplot(data=data, x='{col1}', y='{col2}')"
+}
+graphs.set(values['Pair Plot'])
+y=10
+for key,value in values.items():
+    tkt.Radiobutton(app, text = key, variable=graphs , value=value).place(x=10,y=y)
+    y += 20
+
+# option menu 1
+col1 = tkt.Variable(app)
+values = ['Select','Product','Age','Gender']
+col1.set(values[0])
+tkt.Label(app, text='Column 1').place(x=100,y=10)
+tkt.OptionMenu(app, col1, *values).place(x=100,y=40)    
+
+# option menu 2
+col2 = tkt.Variable(app)
+col2.set(values[0])
+tkt.Label(app, text='Column 2').place(x=100,y=80)
+tkt.OptionMenu(app, col2, *values).place(x=100,y=110)    
+
+# option menu 3
+col3 = tkt.Variable(app)
+col3.set(values[0])
+tkt.Label(app, text='Column 3').place(x=100,y=150)
+tkt.OptionMenu(app, col3, *values).place(x=100,y=180)
+
+#Canvas
+cnv=tkt.Canvas(app,width=200,height=200)
+cnv.place(x=200,y=100)
+
+# Label
+result=tkt.Variable(app)
+tkt.Label(app, textvariable=result).place(x=300,y=300)
+
+def show():
+
+    global cnv
+    column1 = col1.get()
+    column2 = col2.get()
+    column3 = col3.get()
+
+    g=graphs.get()
+    if 'col1' in g:
+        if column1=='Select':
+            result.set('Column1 must be selected')
+            return
+
+    if 'col2' in g:
+        if column2=='Select':
+            result.set('Column2 must be selected')
+            return
+
+    if 'col3' in g:
+        if column3=='Select':
+            result.set('Column3 must be selected')
+            return                
+
+    fig = plt.figure(figsize=(5,2))
+    eval(graphs.get().format(col1 = column1 , col2 = column2 , col3 = column3))
+    fig.savefig('graph.png')
+    img =ImageTk.PhotoImage(
+        Image.open('graph.png').resize((250,200))
+        )
+
+    
+    cnv.create_image(0,0,anchor=tkt.NW,image=img)
+    
+    # print(col1.get(), col2.get(), col3.get())
+    g= graphs.get()
+    #plt.show()
+
+tkt.Button(app, text='Show', command=show).place(x=400, y = 10)
+
+app.mainloop()
